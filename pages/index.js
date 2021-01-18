ef96161a4fb8d8ec6c7080202b67b4808136cd6c
Shawn Le <ShawnLe007@github.com>
2021-01-17 16:07:30 -0800

    Refactor starter website

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi. I'm Shawn.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          Under construction, with love for Next.js!
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
